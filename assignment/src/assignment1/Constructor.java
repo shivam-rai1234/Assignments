61c7a00c10f2012b5a1ca34b9855e1a8cbbe4d6a
shivam-rai1234 <shivam.rai@kpipartners.com>
2022-09-28 09:14:10 +0530

    Commit assignment

--- assignment/src/assignment1/Constructor.java
+++ assignment/src/assignment1/Constructor.java
@@ -7,25 +7,27 @@ public class Constructor {
 	}
 	
 	public Constructor(int type1, int type2) {
-		int num1= type1;
-		int num2= type2;
+		int number= type1;
+		int number1= type2;
 	}
 	
 	public Constructor(double type1, double type2) {
-		double num1= type1;
-		double num2= type2;
+		double number= type1;
+		double number1= type2;
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Constructor obj= new Constructor();   // default
-		Constructor obj2= new Constructor(1,2);
-		Constructor obj3= new Constructor(22.34,23.567);
-		Constructor obj4= new Constructor();
-		Constructor obj5= new Constructor(22,33);
+		Constructor object= new Constructor();   // default
+		Constructor object2= new Constructor(1,2);
+		Constructor object3= new Constructor(22.34,23.567);
+		Constructor object4= new Constructor();
+		Constructor object5= new Constructor(22,33);
+		
 		
 
 
 
-}
+	}
+
