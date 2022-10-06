0ca0414b1deedfeb96962d39c25f17092e1e7d9e
shivam-rai1234 <shivam.rai@kpipartners.com>
2022-10-06 09:37:45 +0530

    commit

--- assignment/src/assignment3/FunInterface2.java
+++ assignment/src/assignment3/FunInterface2.java
@@ -2,18 +2,17 @@ package assignment3;
 //here we create functional interface with the help of lambda function
 
 public	class FunInterface2 {
+		
 		public static void main(String args[])
-		{
-			// create anonymous inner class object
-			new Thread(new Runnable() 
-			{
-				@Override public void run()
-				{
-					System.out.println("New thread created");
-				}
-			}
-			).start();
-		}
+	    {
+	  
+	        // lambda expression to create the object
+	        new Thread(() -> 
+	        {
+	            System.out.println("New thread created");
+	        }
+	        				).start();
+	    }
 	}
 
 
